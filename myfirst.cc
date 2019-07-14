c4c3ae39542af69f7a74fbe75ba075dea1d85816
jinhui <jinhuis2@illinois.edu>
2019-07-14 11:19:38 -0500

    Myfirst to first.

--- myfirst.cc
+++ myfirst.cc
@@ -21,7 +21,6 @@
 #include "ns3/applications-module.h"
 
 using namespace ns3;
-using namespace std;
 
 NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
 
@@ -36,49 +35,31 @@ main (int argc, char *argv[])
   LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
   NodeContainer nodes;
-  nodes.Create (3);
+  nodes.Create (2);
 
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-  NetDeviceContainer dev1 ,dev2;
-  dev1 = pointToPoint.Install (nodes.Get(0), nodes.Get(1));
-  dev2 = pointToPoint.Install (nodes.Get(1), nodes.Get(2));
-
-  cout << "Address from devices: " << endl;
-  for (uint32_t i = 0; i < nodes.GetN(); i ++)
-  {
-    for (uint32_t j = 0; j < nodes.Get(i)->GetNDevices(); j ++)
-      cout << nodes.Get(i)->GetDevice(j)->GetAddress() << endl;
-  }
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install (nodes);
 
   InternetStackHelper stack;
   stack.Install (nodes);
 
-  Ipv4AddressHelper address, addr2;
+  Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
-  addr2.SetBase ("10.2.1.0", "255.255.255.0");
 
-  Ipv4InterfaceContainer interfaces;
-  interfaces.Add (address.Assign (dev1) );
-  // address.NewNetwork();
-  interfaces.Add (address.Assign (dev2) );
-  cout << "Interfaces: " << endl;
-  for (uint32_t i = 0; i < interfaces.GetN(); i ++)
-  {
-    interfaces.GetAddress(i).Print(cout);
-    cout << endl;
-  }
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
   UdpEchoServerHelper echoServer (9);
 
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));   // Get(1)
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);          // GetAddress(1)
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (2));
+  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
@@ -86,11 +67,10 @@ main (int argc, char *argv[])
   clientApps.Start (Seconds (2.0));
   clientApps.Stop (Seconds (10.0));
 
-
   AsciiTraceHelper ascii;
   pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("myfirst.tr"));
   pointToPoint.EnablePcapAll ("myfirst");
-  
+
   Simulator::Run ();
   Simulator::Destroy ();
   return 0;
